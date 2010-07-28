2652442637335e6d91f5d24d9c3260623c272218
Bo Ørsted Andresen <zlin@exherbo.org>
2010-07-28 10:53:01 +0000

    Add script for regenerating summer and sending mails upon failure.

---
+++ regenerate_summer.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+run_date=$(date -u +%Y-%m-%d_%H:%M)
+RECIPIENTS="exherbo-job-logs@exherbo.org"
+LOG="${HOME}/nightly-out/summer-${run_date}.log"
+if [[ -n ${1} ]]; then
+    ALWAYS_SENDMAIL=yesplease
+fi
+
+mail() {
+    local subject="${1}" body="${2}"
+    HOME="/tmp" LANG="en_GB.utf8" EMAIL="Statically Updated Metadata Manifestation for Exherbo Repositories <summer@git.exherbo.org>" \
+        mutt -s '${subject}' -- ${RECIPIENTS}
+}
+
+die() {
+    echo "${1}" >&2
+    mail "Failure: Summer regeneration ${run_date}" "${LOG}"
+    exit 1
+}
+
+cd ~/summer.git &>>"${LOG}" || die "Entering summer.git failed"
+make clean &>>"${LOG}" || die "make clean failed"
+make &>>"${LOG}" || die "make failed"
+make upload &>>"${LOG}" || die "make upload failed"
+
+[[ -n ${ALWAYS_SENDMAIL} ]] && mail "Success: Summer regeneration ${run_date}" "${LOG}"
+
